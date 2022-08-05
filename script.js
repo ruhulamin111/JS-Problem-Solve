f322f4167b470107ab1d2a91f9d2fd8b406c3653
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-05 12:14:05 +0600

    electricity bill added

--- script.js
+++ script.js
@@ -28,7 +28,13 @@ console.log(result);
  */
 
 // 3. calculate the price of book price less than or equal 5 kg it's per price 2000 and more than 5 kg it's price 1500 ; 
-function buyBook(quantity) {
+/* function buyBook(quantity) {
+    if (typeof quantity != 'number') {
+        return 'Please input a number'
+    }
+    else if (quantity <= 0) {
+        return 'Please buy minimum one book'
+    }
     let price = 0;
     if (quantity <= 5) {
         price = quantity * 2000;
@@ -38,5 +44,28 @@ function buyBook(quantity) {
     }
     return price;
 }
-const result = buyBook(5);
+const result = buyBook(0);
+console.log(result); */
+
+// 3. calculate the price of electricity bill first 100 kw price is 5 tk and more than 100 kw then the price is 6tk  per unit 
+function billOfElectricity(unit) {
+    if (typeof unit != 'number') {
+        return 'Please input a number'
+    }
+    else if (unit <= 0) {
+        return 'Please input a positive number'
+    }
+    let bill = 0;
+    if (unit <= 100) {
+        bill = unit * 5;
+    }
+    else {
+        billPrevious = 100 * 5;
+        restUnit = unit - 100;
+        billUpdate = restUnit * 6;
+        bill = billPrevious + billUpdate;
+    }
+    return bill;
+}
+const result = billOfElectricity(120);
 console.log(result);
